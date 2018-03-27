82f6bcb913d2c5eef8f4bcec5be9ff74c1c52236
CR <rajshekhar@protonmail.com>
2018-03-27 14:18:36 -0500

    scope (Variable )accessbility ...

---
+++ Scope.js
@@ -0,0 +1,12 @@
+//The term scope describes an area in which variables and functions, or identifiers, can be accessed.
+
+var globalVariable = 'This is global'; //can be accessed to anywhere in the file...even inside below two funcitons
+
+function globalFunction1() { //funtion can be accessed..like global bvariable
+
+  var innerVariable1 ='Non-global variable 1';//child scopes of global variable....can only be accessed here in this function only..like abstartcion
+}
+
+function globalFunction2() {
+  var innerVariable2 = 'Non-global variable 2';//child scopes of global variable....can only be accessed here in this function only..like abstartcion ..like abstraction
+}
