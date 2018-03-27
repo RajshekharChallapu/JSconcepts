41193e7f9d922268cc6b3e953273a5e394e87013
CR <rajshekhar@protonmail.com>
2018-03-27 12:01:56 -0500

    global variable...access to other function methods

---
+++ selfCallingFunc.js
@@ -0,0 +1,18 @@
+//Also Called Self-invoked Anonymous Function or Immediately Invoked Function Expression(IIFE).
+
+//It is a function that executes automatically when we create it
+
+//Initialization starts
+
+var counter =10;  //global variable
+counter ++;
+//ends here
+
+function SomeFun(){
+    counter= counter +1; //someFun
+
+}
+SomeFun();   //invoking
+
+
+
