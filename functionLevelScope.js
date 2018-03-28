e763119ee154d62ecc82d21d79c068694b39bbc3
CR <rajshekhar@protonmail.com>
2018-03-28 01:32:40 -0500

    block scope using if statment ..

--- functionLevelScope.js
+++ functionLevelScope.js
@@ -16,3 +16,19 @@ console.log (name); //Am global variable : the global variable
 
 
 
+
+
+/*No Block-Level Scope*/
+var name = "Am global";
+​// the blocks in this if statement do not create a local context for the name variable​
+
+​if (name) {
+	name = "Am block scope at global level you can change me"; // this name is the global name variable and it is being changed here​
+
+
+	console.log (name); // Am block scope at global level you can change me: still the global variable​
+}
+​
+​// Here, the name variable is the same global name variable, but it was changed in the if statement​
+console.log (name); // Am block scope at global level you can change me
+
