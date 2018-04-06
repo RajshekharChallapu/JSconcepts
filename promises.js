de6f510a92b1f018b57563eb424c1b063d69e661
CR <rajshekhar@protonmail.com>
2018-04-06 02:20:57 -0500

    at a time to check all the promises fullfilled or rejected

--- promises.js
+++ promises.js
@@ -2,28 +2,29 @@
 
 let cleanRoom = function () {
     return new Promise(function(resolve, reject) {
-
         resolve('Cleaned the room');
-
     });
 };
 
-let removeGarbage = function (message) {
-    return new Promise(function (resolve, reject) {
+let removeGarbage = function(message) {
+    return new Promise(function(resolve, reject) {
         resolve( message + 'remove garbage');
     });
 };
 
 let winIcecream = function (message) {
-    return new Promise(function (resolve, reject) {
+    return new Promise(function(resolve, reject) {
         resolve(message + 'won Icecream');
     });
 };
 
-cleanRoom().then(function(result){
-    return removeGarbage(result);
-}).then(function(result){
-    return winIcecream(result);
-}).then(function(result){
-    console.log('finished' + result);
-})
+Promise.all([cleanRoom(),removeGarbage(),winIcecream()]).
+then(function(){
+    console.log('all finished')
+
+});
+/*Promise.race([cleanRoom(),removeGarbage(),winIcecream()]).
+then(function(){
+    console.log('one of them finished')
+
+});*/
