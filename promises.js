5230f1613142e1a2066dec4c87ee1fd2a541f3a4
CR <rajshekhar@protonmail.com>
2018-04-06 02:09:07 -0500

    independent promises

--- promises.js
+++ promises.js
@@ -1,22 +1,29 @@
-                                          //callback function
-let promiseToFinishTheGame =  new Promise(function(resolve, reject){
-    
-    //Finish the Game
-    let isFinish =false;
-    
-    if(isFinish){
-        resolve('Finish');
-        
-    }else{
-        reject('not clean');
-    }
-});            
-//calling promise to check is resolved//callback
-promiseToFinishTheGame.then(function(fromResolve){
-    
-    console.log('the room is' + fromResolve );
-    // to check error if reject
-}).catch(function(fromReject){
-    console.log('the room is' + fromReject);
-    
-});
+// nested and independent promises
+
+let cleanRoom = function () {
+    return new Promise(function(resolve, reject) {
+
+        resolve('Cleaned the room');
+
+    });
+};
+
+let removeGarbage = function (message) {
+    return new Promise(function (resolve, reject) {
+        resolve( message + 'remove garbage');
+    });
+};
+
+let winIcecream = function (message) {
+    return new Promise(function (resolve, reject) {
+        resolve(message + 'won Icecream');
+    });
+};
+
+cleanRoom().then(function(result){
+    return removeGarbage(result);
+}).then(function(result){
+    return winIcecream(result);
+}).then(function(result){
+    console.log('finished' + result);
+})
