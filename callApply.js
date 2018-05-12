7274ae78f9a7104a5acc5c701f34f09530969279
CR <rajshekhar@protonmail.com>
2018-05-12 03:12:23 -0500

    call and apply

---
+++ callApply.js
@@ -0,0 +1,16 @@
+//ex: Obj1 and Obj 2 can have separate methods by following functions args..where call, apply, and bind methods
+
+//building object
+var obj={num:2}; // property num
+//method
+var addToThis = function(a, b,c){
+    return this.num+a+b+c;
+};
+
+//console.log(addToThis.call(obj, 4,5,6));//functionname.call(obj, function arguments)//first argumrnt would be obj
+
+//apply
+var arr = [1,2,3]
+comsole.log(addToThis.apply(obj,arr));
+
+
