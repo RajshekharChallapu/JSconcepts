1be531872a45298766fdb39177d38d5adecdba6f
CR <rajshekhar@protonmail.com>
2018-05-12 22:08:20 -0500

    bind calling function

--- bind.js
+++ bind.js
@@ -1,8 +1,41 @@
-
+//Bind allows us to borrow methods
+// In JS we can pass functions around,return them, borrow them ..and bind() method makes it easy to borrow methods
 var obj ={num :2};
 var addToThis = function(a,b,c){
     return this.num +a+b+c;
 };
 var arr = [1,2,3];
-//return function
-console.log(addToThis.bind(obj,arr));
+var bound = addToThis.bind(obj);
+console.log(bound(2,4,6));
+
+
+// Example 2
+// here we have cars objects that does
+not have method to print its data to the console
+
+var cars={
+    data:[
+        {name:"Honda Accord", age:23},
+        {name: "Tesla", age:2}
+    ]
+}
+
+
+//here we bind the users.showData method to the cars object we just created.
+cars.showData =user.showData.bind(cars);
+cars.showData();// honda Accord 14
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
