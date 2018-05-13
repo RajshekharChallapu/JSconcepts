c08339d31cf68c9b3928a61e5d0bf8fc9b198c47
CR <rajshekhar@protonmail.com>
2018-05-12 22:18:08 -0500

    call , apply info

--- callApply.js
+++ callApply.js
@@ -1,5 +1,5 @@
 //call() and apply() serve the exact same purpose. The only difference between how they work is that call() expects all parameters to be passed in individually, whereas apply() expects an array of all of our parameters.
-
+//The most common use for the Apply and Call methods in JavaScript is probably to borrow functions. We can borrow functions with the Apply and Call methods
 var pokemon = {
     firstname: 'Pika',
     lastname: 'Chu ',
@@ -18,3 +18,5 @@ Executes the function it was called upon right away.
 The call() method does not make a copy of the function it is being called on.
 pokemonName.call(pokemon,'sushi', 'algorithms'); // Pika Chu  loves sushi and algorithms
 pokemonName.apply(pokemon,['sushi', 'algorithms']); // Pika Chu  loves sushi and algorithms
+
+//The Apply and Call methods are two of the most often used Function methods in JavaScript, and for good reason: they allow us to borrow functions and set the this value in function invocation. In addition, the apply function in particular allows us to execute a function with an array of parameters, such that each parameter is passed to the function individually when the function executes—great for variadic functions; a variadic function takes varying number of arguments, not a set number of arguments as most functions do
