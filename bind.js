ebaaaf0513a8d5d22cb46bb28d081fa023ef1be0
CR <rajshekhar@protonmail.com>
2018-05-12 22:12:00 -0500

    bind complete

--- bind.js
+++ bind.js
@@ -1,32 +1,27 @@
 //Bind allows us to borrow methods
 // In JS we can pass functions around,return them, borrow them ..and bind() method makes it easy to borrow methods
-var obj ={num :2};
-var addToThis = function(a,b,c){
-    return this.num +a+b+c;
+
+
+
+var pokemon = {
+    firstname: 'Pika',
+    lastname: 'Chu ',
+    getPokeName: function() {
+        var fullname = this.firstname + ' ' + this.lastname;
+        return fullname;
+    }
 };
-var arr = [1,2,3];
-var bound = addToThis.bind(obj);
-console.log(bound(2,4,6));
+
+var pokemonName = function() {
+    console.log(this.getPokeName() + 'I choose you!');
+};
+
+var logPokemon = pokemonName.bind(pokemon); // creates new object and binds pokemon. 'this' of pokemon === pokemon now
+
+logPokemon(); // 'Pika Chu I choose you!'
 
 
-// Example 2
-// here we have cars objects that does
-not have method to print its data to the console
-
-var cars={
-    data:[
-        {name:"Honda Accord", age:23},
-        {name: "Tesla", age:2}
-    ]
-}
-
-
-//here we bind the users.showData method to the cars object we just created.
-cars.showData =user.showData.bind(cars);
-cars.showData();// honda Accord 14
-
-
-
+//Let’s break it down. When we use the bind() method:the JS engine is creating a new pokemonName instance and binding pokemon as its this variable. It is important to understand that it copies the pokemonName function.After creating a copy of the pokemonName function it is able to call logPokemon(), although it wasn’t on the pokemon object initially. It will now recognizes its properties (Pika and Chu) and its methods.
 
 
 
