775c6863ee1ab57ff7cd9314ad21556aa2560072
CR <rajshekhar@protonmail.com>
2018-06-04 00:19:23 -0500

    event emitter

---
+++ eventEmitter.js
@@ -0,0 +1,30 @@
+//eventEmitter//ES6
+
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+        on(eventName, callback) {
+            if (this.events[eventName]) {
+                this.events[eventName].push(callback);
+            } else {
+                this.events[eventName] = [callback];
+            }
+        }
+        trigger(eventName, ...rest) {
+            if (this.events[eventName]) {
+                this.events[eventName].forEach(cb =>{
+cb.apply(rest);
+
+
+                });
+            }
+
+        }
+    }
+
+const ee= new EventEmitter();
+ee.on('change',()=>{
+    console.log('hello there!');
+});
+ee.trigger('change');
